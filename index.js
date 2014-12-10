06ff63f1403cd1082a1d5f342829377be145a29b
psirenny <djtorres0@gmail.com>
2014-12-10 10:28:46 -0500

    fix bug when there is no user

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ module.exports = function (options) {
   if (!options.key) options.key = 'local.locales';
   if (!options.path) options.path = '$locale';
   if (!options.name) options.name = 'user';
+
   return function (req, res, next) {
     var model = req.getModel();
+    if (!req.isAuthenticated()) return next();
     var $user = model.at('users.' + req.user.id);
     $user.fetch(function (err) {
       if (err) return next(err);
       var path = options.path + '.strategies.' + options.name;
       var strategy = {};
-      strategy.locales = $user.setNull(options.key, [])
+      strategy.locales = $user.setNull(options.key, []);
       strategy.order = options.order;
       model.set(path, strategy);
       next();
